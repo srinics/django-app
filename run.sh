61196b3f517193f03ea53c672e72a74cd102aa86
Srini R <srinics@hotmail.com>
2020-04-10 03:46:37 +0000

    Added docker file

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ CONTAINER_PORT=9000
 
 REPONAME="srinics"
 
-DB_IMAGE_NAME="postgres"
-IMAGE_NAME="django-project-img"
-CONTAINER_NAME="django-project-cnt"
+IMAGE_NAME_DB="postgres"
+IMAGE_NAME_WEB="django-project-img"
+CONTAINER_NAME_WEB="django-project-cnt"
 OP=$1
 
 
@@ -20,22 +20,22 @@ elif [ "$OP" = "c" ]; then
 	CLEAN=1
 fi
 
-docker rmi $IMAGE_NAME --force
-docker rmi $DB_IMAGE_NAME --force
+docker rmi $IMAGE_NAME_WEB --force
+docker rmi $IMAGE_NAME_DB --force
 docker image prune --force
 docker container prune --force
-docker kill $CONTAINER_NAME
+docker kill $CONTAINER_NAME_WEB
 
 if [ $CLEAN ]; then
 	exit 0
 fi
 
-docker build $DOCKER_BUILD_OPTS -t $IMAGE_NAME . || exit
+docker build $DOCKER_BUILD_OPTS -t $IMAGE_NAME_WEB . || exit
 
 if [ $DEMONIZE ]; then
-	docker run --name $CONTAINER_NAME -p $HOST_PORT:$CONTAINER_PORT -itd --env http_proxy=$HTTP_PROXY --env https_proxy=$HTTPS_PROXY $IMAGE_NAME || exit
+	docker run --name $CONTAINER_NAME_WEB -p $HOST_PORT:$CONTAINER_PORT -itd --env http_proxy=$HTTP_PROXY --env https_proxy=$HTTPS_PROXY $IMAGE_NAME_WEB || exit
 else
-	docker run --name $CONTAINER_NAME -p $HOST_PORT:$CONTAINER_PORT -it --env http_proxy=$HTTP_PROXY --env https_proxy=$HTTPS_PROXY $IMAGE_NAME || exit
+	docker run --name $CONTAINER_NAME_WEB -p $HOST_PORT:$CONTAINER_PORT -it --env http_proxy=$HTTP_PROXY --env https_proxy=$HTTPS_PROXY $IMAGE_NAME_WEB || exit
 fi
 
 docker ps
