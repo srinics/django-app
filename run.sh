7db4c45a0b31280a857a42b36a913c98406d7126
Srini R <srinics@hotmail.com>
2020-04-10 06:37:46 +0000

    Modified run.sh

--- run.sh
+++ run.sh
@@ -3,18 +3,21 @@ HTTP_PROXY="http://localproxy:12000/"
 HTTPS_PROXY="http://localproxy:12000/"
 #DOCKER_BUILD_OPTS="--build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTPS_PROXY"
 
-HOST_PORT=9000
-CONTAINER_PORT=9000
+HOST_PORT_APP=9000
+CONTAINER_PORT_APP=9000
+HOST_PORT_DB=5432
+CONTAINER_PORT_DB=5432
 
 REPONAME="srinics"
-
 TPATH="./docker/temp/"
 
+ENV_FILE_DB="./docker/db.env"
 DOCKERFILE_APP="./docker/dockerfile-app"
 DOCKERFILE_DB="./docker/dockerfile-db"
 IMAGE_NAME_DB="dproject-db-img"
 IMAGE_NAME_APP="dproject-app-img"
 CONTAINER_NAME_APP="dproject-app-cnt"
+CONTAINER_NAME_DB="dproject-db-cnt"
 OP=$1
 
 
@@ -39,9 +42,11 @@ docker build $DOCKER_BUILD_OPTS -t $IMAGE_NAME_DB -f $DOCKERFILE_DB $TPATH || ex
 docker build $DOCKER_BUILD_OPTS -t $IMAGE_NAME_APP -f $DOCKERFILE_APP . || exit
 
 if [ $DEMONIZE ]; then
-	docker run --name $CONTAINER_NAME_APP -p $HOST_PORT:$CONTAINER_PORT -itd --env http_proxy=$HTTP_PROXY --env https_proxy=$HTTPS_PROXY $IMAGE_NAME_APP || exit
+	docker run --name $CONTAINER_NAME_DB -p $HOST_PORT_DB:$CONTAINER_PORT_DB  -itd --env http_proxy=$HTTP_PROXY --env https_proxy=$HTTPS_PROXY --env-file $ENV_FILE_DB $IMAGE_NAME_DB || exit
+	docker run --name $CONTAINER_NAME_APP -p $HOST_PORT_APP:$CONTAINER_PORT_APP -itd --env http_proxy=$HTTP_PROXY --env https_proxy=$HTTPS_PROXY $IMAGE_NAME_APP || exit
 else
-	docker run --name $CONTAINER_NAME_APP -p $HOST_PORT:$CONTAINER_PORT -it --env http_proxy=$HTTP_PROXY --env https_proxy=$HTTPS_PROXY $IMAGE_NAME_APP || exit
+	docker run --name $CONTAINER_NAME_DB -it -p $HOST_PORT_DB:$CONTAINER_PORT_DB   --env http_proxy=$HTTP_PROXY --env https_proxy=$HTTPS_PROXY --env-file $ENV_FILE_DB $IMAGE_NAME_DB || exit
+	docker run --name $CONTAINER_NAME_APP -p $HOST_PORT_APP:$CONTAINER_PORT_APP -it --env http_proxy=$HTTP_PROXY --env https_proxy=$HTTPS_PROXY $IMAGE_NAME_APP || exit
 fi
 
 docker ps
