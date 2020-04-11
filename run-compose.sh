b0edd48741f810aaa5c2231643be30a69f2c8587
Srini R <srinics@hotmail.com>
2020-04-11 07:36:14 +0000

    Modified run-compose.sh

--- run-compose.sh
+++ run-compose.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+set -x
 OP=$1
 
 if [ "$OP" = "d" ]; then
 	docker-compose -f ./docker/docker-compose.yml --project-dir=.  -d up
 elif [ "$OP" = "c" ]; then
-	docker-compose -f ./docker/docker-compose.yml stop
-	docker-compose -f ./docker/docker-compose.yml kill
+	docker-compose -f ./docker/docker-compose.yml --project-dir=.  stop
+	docker-compose -f ./docker/docker-compose.yml --project-dir=.  kill
 elif [ "$OP" = "t" ]; then
  	curl http://localhost:9000/index	
 fi
 
-docker-compose -f ./docker/docker-compose.yml --project-dir=.  up
+if [ -z "$OP" ]; then
+	docker-compose -f ./docker/docker-compose.yml --project-dir=.  up
+fi
+exit 0
+
