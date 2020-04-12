0c4229d1a25c81bd0d5a61f9323ead1074ad92bb
Srini R <srinics@hotmail.com>
2020-04-12 10:50:26 +0000

    Added kompose output

--- run-kompose.sh
+++ run-kompose.sh
@@ -2,3 +2,4 @@
 curl -L https://github.com/kubernetes/kompose/releases/download/v1.21.0/kompose-linux-amd64 -o kompose
 chmod +x kompose
 sudo mv ./kompose /usr/local/bin/kompose
+kompose --file ./docker/docker-compose-kompose.yml convert
